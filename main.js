7bba52051e76c5dae9b7691ac89fcc6f64a77730
NwaguChima <nwaguchima48@gmail.com>
2024-03-24 22:07:43 +0100

    added success

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 const progressBar = document.querySelector('.progressbar');
+let progress = 0;
 
 function enableProgressBar() {
   progressBar.setAttribute('role', 'progressbar');
-  progressBar.setAttribute('aria-valuenow', 0);
+  progressBar.setAttribute('aria-valuenow', progress);
   progressBar.setAttribute('aria-valuemin', 'polite');
 }
 
 enableProgressBar();
+
+const testingBtn = document.querySelector('.testing-ground');
+
+testingBtn.addEventListener('click', (e) => {
+  if (!e.target.closest('button')) return;
+
+  progress = e.target.dataset.progress;
+  updateProgress(progress);
+});
+
+function updateProgress(progress) {
+  progressBar.setAttribute('aria-valuenow', progress);
+  progressBar.style.setProperty('--progress', progress + '%');
+}
+
+function simulateUpload() {
+  // prevent announcing every update interval
+  progress.setAttribute('aria-busy', 'true');
+  let progress = 0;
+  updateProgress(progress);
+
+  intervalTimer = setInterval(() => {
+    progress += 5;
+    updateProgress(progress);
+    if (progress === 100) progressBar.setAttribute('aria-busy', 'false');
+    clearInterval(intervalTimer);
+  }, 500);
+}
